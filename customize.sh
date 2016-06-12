ac00502a4327c4c298418acedf04630651f75711
Carl M. Lane <carlmlane@gmail.com>
2016-06-12 10:23:06 -0700

    added build script for tmux 2.2

--- customize.sh
+++ customize.sh
@@ -13,9 +13,12 @@ cp .vimrc ~/
 # install tmux 2
 sudo apt-get update
 sudo apt-get install -y python-software-properties software-properties-common
-sudo add-apt-repository -y ppa:pi-rho/dev
-sudo apt-get update
-sudo apt-get install -y tmux
+cd ~
+wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
+tar -zxf tmux-2.2.tar.gz
+cd tmux-2.2
+./configure && make
+sudo ln -s /home/$(whoami)tmux-2.2/tmux /usr/local/bin/tmux
 
 # install tmux package manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
