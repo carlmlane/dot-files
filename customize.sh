62d6c176482fe7ff4d410c079bf886ea3f5fa71b
Carl M. Lane <carl@wix.com>
2016-08-15 17:03:57 -0700

    forgot a /

--- customize.sh
+++ customize.sh
@@ -21,7 +21,7 @@ wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
 tar -zxf tmux-2.2.tar.gz
 cd tmux-2.2
 ./configure && make
-sudo ln -s /home/$(whoami)tmux-2.2/tmux /usr/local/bin/tmux
+sudo ln -s /home/$(whoami)/tmux-2.2/tmux /usr/local/bin/tmux
 
 # install tmux package manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
